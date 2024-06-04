70d8afc6c43924a7a6ead6fd1269efbae0d24bdd
Aaron Bushnell <aaron.bushnell@gmail.com>
2024-06-03 23:49:33 -0400

    Format

--- src/_scripts/alpine.js
+++ src/_scripts/alpine.js
@@ -5,14 +5,20 @@ window.Alpine = Alpine
 // Start Alpine when the page is ready.
 window.addEventListener('DOMContentLoaded', () => {
   Alpine.start()
-});
+})
 
 // Basic Store Example in Alpine.
 window.addEventListener('alpine:initializing', () => {
   Alpine.store('nav', {
     isOpen: false,
-    close() { return this.isOpen = false },
-    open() { return this.isOpen = true },
-    toggle() { return this.isOpen = !this.isOpen }
+    close() {
+      return (this.isOpen = false)
+    },
+    open() {
+      return (this.isOpen = true)
+    },
+    toggle() {
+      return (this.isOpen = !this.isOpen)
+    },
   })
-});
+})
