7855e6a00f4f5fd8c21d3ab6aba9dfd55e3b4108
Aaron Bushnell <aaron.bushnell@gmail.com>
2024-06-03 23:50:33 -0400

    Disable Alpine until we need it

--- src/_scripts/alpine.js
+++ src/_scripts/alpine.js
@@ -1,24 +1,8 @@
-import Alpine from 'alpinejs'
+// import Alpine from 'alpinejs'
 
-window.Alpine = Alpine
+// window.Alpine = Alpine
 
-// Start Alpine when the page is ready.
-window.addEventListener('DOMContentLoaded', () => {
-  Alpine.start()
-})
-
-// Basic Store Example in Alpine.
-window.addEventListener('alpine:initializing', () => {
-  Alpine.store('nav', {
-    isOpen: false,
-    close() {
-      return (this.isOpen = false)
-    },
-    open() {
-      return (this.isOpen = true)
-    },
-    toggle() {
-      return (this.isOpen = !this.isOpen)
-    },
-  })
-})
+// // Start Alpine when the page is ready.
+// window.addEventListener('DOMContentLoaded', () => {
+//   Alpine.start()
+// })
