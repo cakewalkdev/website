e0d969240af7921ae2efc98640766234e16ca34b
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2024-06-03 20:48:01 -0400

    Moving homepage stuff over

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,41 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,md,twig,svg}'],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('tailwindcss-debug-screens'),
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: '1.5rem',
-    },
-    debugScreens: {
-      position: ['bottom', 'right'],
-    },
-    extend: {},
-  },
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+		debugScreens: {
+			position: ['bottom', 'right'],
+			style: {
+				backgroundColor: '#262626e6',
+				borderRadius: '4px',
+				boxShadow: 'none',
+				color: 'white',
+				padding: '8px',
+				margin: '18px 16px',
+				opacity: '100%',
+			},
+		},
+		screens: {
+			xs: '480px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+		extend: {
+			colors: {
+				green: '#52FFB8',
+				navy: '#232743',
+				'dark-navy': '#191b30',
+				'black-navy': '#0f101e',
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('tailwindcss-debug-screens'),
+	],
 }
