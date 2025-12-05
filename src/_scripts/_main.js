96dec34c62e52ce44d3c101ae33146fff6967de7
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2025-12-04 19:31:22 -0500

    AOS baby

--- src/_scripts/_main.js
+++ src/_scripts/_main.js
@@ -1,1 +1,1 @@
-import './alpine'
+import './window'
--- src/_scripts/window.js
+++ src/_scripts/window.js
@@ -1,8 +1,11 @@
 import Alpine from 'alpinejs'
 import { collapse } from '@alpinejs/collapse'
+import AOS from 'aos'
+
+AOS.init({
+  once: true,
+})
 
 Alpine.plugin(collapse)
-
 window.Alpine = Alpine
-
 Alpine.start()
